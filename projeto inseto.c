55fd0d600aa68b3b1dbf3163500c9fc1c0b94174
Lukeladin <92125680+Lukeladin@users.noreply.github.com>
2023-10-17 08:26:29 -0300

    Add files via upload

---
+++ projeto inseto.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+int pergunta(int op1, int op2);
+
+main(){
+    int pergunta[22], i;
+    char enunciado[22][178]
+}
+int pergunta(int op1, int op2){
+    char resposta;
+
+}
